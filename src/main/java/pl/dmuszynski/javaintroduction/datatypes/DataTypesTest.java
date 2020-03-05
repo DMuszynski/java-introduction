4c71674e30b826dc0de3d259525324a837443d69
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-03-05 15:37:20 +0100

    add DataTypesTest class

---
+++ src/main/java/pl/dmuszynski/javaintroduction/datatypes/DataTypesTest.java
@@ -0,0 +1,68 @@
+package pl.dmuszynski.javaintroduction.datatypes;
+
+public class DataTypesTest {
+
+    // primitive data types
+    private boolean a = false;
+    private char b = 'b';
+    private byte c = 1;
+    private short d = 2;
+    private int e = 3;
+    private long f = 4L;
+    private float g = 5f;
+    private double h = 6d;
+
+    // non-primitive data types
+    private Boolean a1 = false;
+    private Character b1 = 'b';
+    private Byte c1 = 1;
+    private Short d1 = 2;
+    private Integer e1 = 3;
+    private Long f1 = 4L;
+    private Float g1 = 5f;
+    private Double h1 = 6d;
+
+    private void testBooleanDataTypes() {
+
+    }
+
+    private void testCharDataTypes() {
+
+    }
+
+    private void testByteDataTypes() {
+
+    }
+
+    private void testShortDataTypes() {
+
+    }
+
+    private void testIntDataTypes() {
+
+    }
+
+    private void testLongDataTypes() {
+
+    }
+
+    private void testFloatDataTypes() {
+
+    }
+
+    private void testDoubleDataTypes() {
+
+    }
+
+    public static void main(String[] args) {
+        DataTypesTest app = new DataTypesTest();
+        app.testBooleanDataTypes();
+        app.testCharDataTypes();
+        app.testByteDataTypes();
+        app.testShortDataTypes();
+        app.testIntDataTypes();
+        app.testLongDataTypes();
+        app.testFloatDataTypes();
+        app.testDoubleDataTypes();
+    }
+}
